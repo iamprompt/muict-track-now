34c2a8693a8efb4afe4cd9301ba09e2f021bad09
Supakarn Laorattanakul <wearedprompt@gmail.com>
2021-11-03 03:14:49 +0700

    LINE Notify

--- index.ts
+++ index.ts
@@ -2,11 +2,13 @@ import * as puppeteer from 'puppeteer'
 import { config as dotenv } from 'dotenv'
 import { writeFileSync } from 'fs'
 import axios from 'axios'
+import { stringify } from 'query-string'
 
 dotenv()
 
 const MU_USERNAME = process.env.MU_USERNAME || ''
 const MU_PASSWORD = process.env.MU_PASSWORD || ''
+const LINE_NOTIFY_TOKEN = process.env.LINE_NOTIFY_TOKEN || ''
 
 ;(async () => {
   const browser = await puppeteer.launch()
@@ -25,10 +27,30 @@ const MU_PASSWORD = process.env.MU_PASSWORD || ''
   await Promise.all([page.waitForNavigation(), page.click('#btnLogin')])
 
   // Get Major Text
-  const NewMajor = await page.$eval('#ContentPlaceHolder1_lblMajorENm', (e) => e.textContent)
-  console.log(NewMajor)
+  const newMajor = await page.$eval('#ContentPlaceHolder1_lblMajorENm', (e) => e.textContent)
+  console.log(newMajor)
 
-  writeFileSync('README.md', `${NewMajor}|${new Date().toLocaleString()}`)
+  let { data: oldMajor } = await axios.get('https://raw.githubusercontent.com/iamprompt/muict-track-now/main/README.md')
+
+  oldMajor = oldMajor.split('|')[0]
+
+  if (newMajor !== oldMajor) {
+    console.log('new')
+    await axios.post(
+      'https://notify-api.line.me/api/notify',
+      stringify({
+        message: `MUICT Track is available now!!\n at https://academic.ict.mahidol.ac.th/student/e-Payment/Default.aspx\nYour track is ${newMajor}`,
+      }),
+      {
+        headers: {
+          Authorization: `Bearer ${LINE_NOTIFY_TOKEN}`,
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      }
+    )
+  }
+
+  writeFileSync('README.md', `${newMajor}|${new Date().toLocaleString()}`)
 
   await page.close()
   await browser.close()
